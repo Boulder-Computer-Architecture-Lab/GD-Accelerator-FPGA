9e80d4c3b2649a3fa423dbca25621025fdd8b87a
Andrew Ajamian <andrewaajamian@gmail.com>
2026-01-27 14:42:17 -0700

    add model

--- Model/Library/loop_uhatfn.h
+++
@@ -1,6 +0,0 @@
-#include <math.h>
-
-typedef double real;
-
-void loop_uhat(const real tol,const real *trmult_reduced,const real *L,const real L_exp,
-    real *uhat_i,const real *R,const real R_exp);
